0c7cdcf906af7335e19b2f4ad7d3565c3eb5d2d5
choeun <echo@woowahan.com>
2016-09-28 20:44:34 +0900

    12번째 수업자료 업로드

---
+++ 13_javascript/dom.js
@@ -0,0 +1,66 @@
+/**
+* JavaScript
+* 1995년 넷스케이프 네비게이터(Navigator)
+* 브랜든 아이크, 내가 10일안에 브라우저 안에 함수형 언어(Scheme)를 넣어볼게
+* 넣었어요 -> JavaScript
+* Mocha -> LiveScript -> JavaScript
+* JavaScript.... JAVA....
+* 사자와.. 바다사자.. 마케팅...
+* 넷스케이프 네비게이터 7.0에 최적화되어있습니다.
+* 언어가 개판
+* C언어 + Scheme + Perl + Java
+*/
+
+/*
+ * 프로그래밍 언어 기초
+ * 개념 : 변수 (variable)
+ * 의미 : 변할 수 있는 값
+ * 변수명 : 숫자나 특수문자로 시작하지 않는 영문자 + 숫자
+ */
+
+var thisYear = 2017;
+
+/*
+ * 개념 : 형 (type)
+ * 의미 : 이 값의 타입
+ */
+
+/*
+ * type
+ * 5종류
+ * 1. 숫자 (number)
+ * - 숫자는 소숫점도 가능하며, 음수도 가능
+ */
+ var nextYear = 2018;
+
+/*
+ * 2. 문자열 (string)
+ * - 문자열은 쌍따옴표("") 혹은 따옴표('')로 구성함
+ */
+
+var baeminTitle = "배달의민족";
+
+/*
+ * 3. boolean
+ * - true 혹은 false
+ * - 참 거짓을 판별하기 위해서 사용하는 데이터 타입
+ */
+
+var choeunBabo = true;
+var chicken = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**/
